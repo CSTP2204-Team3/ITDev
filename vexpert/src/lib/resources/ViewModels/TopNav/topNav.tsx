5277d69e08b8fe157d69a1719dd8801627ec0611
KelseyVCC <83609351+KelseyVCC@users.noreply.github.com>
2023-02-03 21:14:36 -0800

    added some implementation for the top navigation bar

---
+++ vexpert/src/lib/resources/ViewModels/TopNav/topNav.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import styles from './topNav.module.sass'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
+export default function TopNav(){
+    return (
+        <div className={styles.topNav}>
+            <Link href='/' className={styles.logo}>
+                vExpert
+            </Link>
+            <div className={styles.middleElement}>
+                <Link href='/' className={styles.links}>Categories</Link>
+                <Link href='/' className={styles.links}>Profile</Link>
+                <Link href='/login' className={styles.links}>Appointments</Link>
+            </div>
+            <div className={styles.lastElement}>
+                <SearchOutlinedIcon fontSize='large' htmlColor='#F76C6C'  />
+            </div>
+            
+        </div>
+    )
+}
