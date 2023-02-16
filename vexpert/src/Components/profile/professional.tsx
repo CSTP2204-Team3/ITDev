52a6c6bc02c0932973fe73534acd35282b0cb3d2
KelseyVCC <83609351+KelseyVCC@users.noreply.github.com>
2023-02-16 00:56:11 -0800

    made selectable tags

--- vexpert/src/Components/profile/professional.tsx
+++ vexpert/src/Components/profile/professional.tsx
@@ -7,6 +7,16 @@ import { ImageList, ImageListItem } from "@mui/material";
 import Image from "next/image";
 import { ReactNode } from "react";
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+
+
 interface Props {
     userID: string,
     userName: String,
@@ -21,19 +31,33 @@ export default function ProfessionalUserPage( props: Props){
     
     let [name, setName] = useState("")
     let [about, setAbout] = useState("This is my about section, where I can write a small bio to advertise my services")
-    let email: String = "temp@gmail.com"
+    let email: String = props.userEmail
     let [tempTag1, setTempTag1] = useState("")
     let [tempTag2, setTempTag2] = useState("")
     let [tempTag3, setTempTag3] = useState("")
     let [chipNames, setChipNames] = useState([""])
+    const [checked, setChecked] = React.useState([""]);
+
+    const handleToggle = (value: string) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+    
+        if (currentIndex === -1) {
+          newChecked.push(value);
+        } else {
+          newChecked.splice(currentIndex, 1);
+        }
+    
+        setChecked(newChecked);
+      };
     
 
-    function handleAdd() {
-        let stringToAdd = tempTag1
-        if (chipNames.length > 3){
-            chipNames.push(stringToAdd)
-        }
-    }
+    // function handleAdd() {
+    //     let stringToAdd = tempTag1
+    //     if (chipNames.length > 3){
+    //         chipNames.push(stringToAdd)
+    //     }
+    // }
 
     function uploadPortfolio(){
         //TODO
@@ -41,6 +65,9 @@ export default function ProfessionalUserPage( props: Props){
 
     function save(){
         //TODO
+    }
+    function helpPopup(){
+        
     }
 
     
@@ -56,6 +83,7 @@ export default function ProfessionalUserPage( props: Props){
                     <div></div>
                     <button className={ styles.timeButton }>Time Availablity</button>
                     <button className={ styles.saveButton } onClick={ save } >Save</button>
+                    <button className={ styles.questionButton } onClick={ helpPopup } > <HelpOutlineIcon /> </button>
                 </div>
                 {/* white container that will hold all the information we need */}
                 <div className={ styles.whiteContainer }>
@@ -73,6 +101,41 @@ export default function ProfessionalUserPage( props: Props){
                                 <label htmlFor="email">Email: </label>
                                 <text id="email" className={ styles.email1input }> { email } </text>
                             </div>
+                            {/* specialty tags */}
+                            <div className={ styles.tagsSection }>
+                                Tags: 
+                                {/* We can use this if we want to let the users manually type their own tags */}
+                                {/* <input className={ styles.tags1input } type="text" id="newTag" name="newTag" placeholder="Tag 1" onChange={ e => setTempTag1(e.target.value)} />
+                                <input className={ styles.input } type="text" id="newTag" name="newTag" placeholder="Tag 2" onChange={ e => setTempTag2(e.target.value)} />
+                                <input className={ styles.input } type="text" id="newTag" name="newTag" placeholder="Tag 3" onChange={ e => setTempTag3(e.target.value)} /> */}
+                                {/* Or we can use this if we want to make a selectable list */}
+                                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                                    {["Hair Styling", "Hair Cuts", "Hair Colour"].map((value) => {
+                                        const labelId = `checkbox-list-label-${value}`;
+
+                                        return (
+                                            <ListItem
+                                                key={value}
+                                                disablePadding
+                                            >
+                                                <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+                                                <ListItemIcon>
+                                                    <Checkbox
+                                                    edge="start"
+                                                    checked={checked.indexOf(value) !== -1}
+                                                    tabIndex={-1}
+                                                    disableRipple
+                                                    inputProps={{ 'aria-labelledby': labelId }}
+                                                    />
+                                                </ListItemIcon>
+                                                <ListItemText id={labelId} primary={`${value}`} />
+                                                </ListItemButton>
+                                            </ListItem>
+                                        );
+                                    })}
+                                </List>
+
+                            </div>
 
                         </div>
                         {/* Container that will hold the photo */}
@@ -80,43 +143,77 @@ export default function ProfessionalUserPage( props: Props){
                             <Image 
                             className={styles.circleImage}
                             src="/duck.jpeg" 
-                            width={100}
-                            height={24}
+                            width={120}
+                            height={120}
                             alt=""/>
                         </div>
+                        
 
                     </div>
-                    {/* specialty tags */}
-                    <div className={ styles.tagsSection }>
-                        Tags: 
-                        <input className={ styles.tags1input } type="text" id="newTag" name="newTag" placeholder="Tag 1" onChange={ e => setTempTag1(e.target.value)} />
-                        <input className={ styles.input } type="text" id="newTag" name="newTag" placeholder="Tag 2" onChange={ e => setTempTag2(e.target.value)} />
-                        <input className={ styles.input } type="text" id="newTag" name="newTag" placeholder="Tag 3" onChange={ e => setTempTag3(e.target.value)} />
+                    
+                    
 
-                    </div>
                     {/* about section */}
                     <div className={ styles.aboutSection }>
                         <div>About: </div>
-                        <textarea rows={3}>
+                        <textarea rows={3} onChange={e => setAbout(e.target.value)}>
 
                         </textarea>
                     </div>
                     {/* Portfolio pictures */}
                     <div className={ styles.portfolioSection }>
-                        Portfolio
-                        <ImageList cols={3} rowHeight={164}>
-                            {/* We will render the images here */}
-                            <ImageListItem>
-                                <Image src="/duck.jpeg" height={100} width={100} alt="">
+                        Portfolio <button className={styles.plusButton}> <AddAPhotoIcon fontSize="medium"/> </button>
+                        <div className={styles.imageBar}>
+                            <ImageList cols={6} rowHeight={164}>
+                                {/* We will render the images here */}
+                                <ImageListItem>
+                                    <Image src="/duck.jpeg" height={120} width={120} alt="">
 
-                                </Image>
-                            </ImageListItem>
-                            <ImageListItem>
-                                <Image src="/duck.jpeg" height={100} width={100} alt="">
+                                    </Image>
+                                </ImageListItem>
+                                <ImageListItem>
+                                    <Image src="/duck.jpeg" height={120} width={120} alt="">
 
-                                </Image>
-                            </ImageListItem>
-                        </ImageList>
+                                    </Image>
+                                </ImageListItem>
+                                {/* <ImageListItem>
+                                    <Image src="/duck.jpeg" height={120} width={120} alt="">
+
+                                    </Image>
+                                </ImageListItem>
+                                <ImageListItem>
+                                    <Image src="/duck.jpeg" height={120} width={120} alt="">
+
+                                    </Image>
+                                </ImageListItem>
+                                <ImageListItem>
+                                    <Image src="/duck.jpeg" height={120} width={120} alt="">
+
+                                    </Image>
+                                </ImageListItem>
+                                <ImageListItem>
+                                    <Image src="/duck.jpeg" height={120} width={120} alt="">
+
+                                    </Image>
+                                </ImageListItem>
+                                <ImageListItem>
+                                    <Image src="/duck.jpeg" height={120} width={120} alt="">
+
+                                    </Image>
+                                </ImageListItem>
+                                <ImageListItem>
+                                    <Image src="/duck.jpeg" height={120} width={120} alt="">
+
+                                    </Image>
+                                </ImageListItem>
+                                <ImageListItem>
+                                    <Image src="/duck.jpeg" height={120} width={120} alt="">
+
+                                    </Image>
+                                </ImageListItem> */}
+                                
+                            </ImageList>
+                        </div>
                     </div>
                 </div>
             </div>
