269f992fcfddce9882e5bd20b8ffb4b16a1a5a86
JamesKim4913 <jamesmall153@gmail.com>
2023-02-17 00:29:56 -0800

    landing and profile Componentization

--- vexpert/pages/profile.tsx
+++ vexpert/pages/profile.tsx
@@ -7,106 +7,19 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import TopNav from '@/Components/TopNav/topNav';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ProfileCom from '@/Components/profileCom'
 
-
-// theme
-const theme = createTheme({
-    palette: {      
-      neutral: {
-        main: '#F76C6C',
-        contrastText: '#FFFFFF',
-      },
-    },
-  });
-  
-  declare module '@mui/material/styles' {
-    interface Palette {
-      neutral: Palette['primary'];
-    }
-  
-    // allow configuration using `createTheme`
-    interface PaletteOptions {
-      neutral?: PaletteOptions['primary'];
-    }
-  }
-  
-  // Update the Button's color prop options
-  declare module '@mui/material/Button' {
-    interface ButtonPropsColorOverrides {      
-      neutral: true;
-    }
-  }
 
 
 export default function Profile() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      name: data.get('name'),
-      email: data.get('email'),
-    });
-  };
-
   return (
     <>    
 
     <div className="background">
 
-    <TopNav/>
+      <TopNav/>
 
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >         
-           
-          <Typography component="h1" variant="h4">
-            Profile
-          </Typography>
-
-          <div className="profileBox">
-         
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="name"
-              label="Name"
-              name="name"
-              autoComplete="name"
-              autoFocus
-            />            
-
-            <Typography variant="h6" padding="20px">
-              xxxxx@email.com
-            </Typography>                   
-          
-          </Box>
-
-          </div>
-
-          <Button
-              style={{backgroundColor: "#F76C6C"}}
-              type="submit"                   
-              color="neutral"              
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Save
-          </Button>
-
-        </Box>
-        
-      </Container>
-      </ThemeProvider>
+      <ProfileCom />
 
     </div>
 
